c49d489b55a7e38e8cc581b783cde1bedc6ccfb0
hasnain17576 <hasnainwasli17@gmail.com>
2025-07-31 21:55:19 +0500

    submit

---
+++ Variable and data type/main.py
@@ -0,0 +1,33 @@
+a =5
+print(a)
+b = 10
+print(b)
+c = a + b
+print(c)
+d = c * 2
+print(d)
+
+
+a = "hasnain"
+print(a)
+b = "khan"
+print(b)
+c = a + " " + b
+print(c)
+
+a=4
+b=" wasli"
+c = str(a) + b
+print(c)  # This will raise an error because you cannot add an integer and a string directly
+list = [1, 2, 3]
+print(list)
+tuple = (1, 2, 3)
+print(tuple)
+set = {1, 2, 3}
+print(set)
+dict = {"name": "hasnain", "age": 25}
+print(dict)
+a = 5
+b = 10
+c = a + b
+print(c)
