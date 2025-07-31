789d1743d904a3ba7f4d9b0f7bb31cbba8cdeaeb
hasnain17576 <hasnainwasli17@gmail.com>
2025-07-31 17:55:56 +0500

    submitt

---
+++ comments & escape sequence 2/main.py
@@ -0,0 +1,5 @@
+# # print("i am a good boy and working in a software house")
+# print("i am a good boy and  \n working in a software house")
+# # do not remove the line of code
+# # i am hasnain
+print("hey", 22,4,5 ,sep="~")
