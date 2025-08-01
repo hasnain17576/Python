ab0a37c995cfc27d5c72320830ec494525dc0c1b
hasnain17576 <hasnainwasli17@gmail.com>
2025-08-01 09:28:10 +0500

    submitt

---
+++ Typecasting in python/main.py
@@ -0,0 +1,29 @@
+# typecasting in python 
+# typecasting is the process of converting one data type to another
+# int to float
+x = 5
+y = float(x)
+print(y)
+# float to int
+z = int(y)
+print(z)
+# int to string
+a = 10
+b = str(a)
+print(b)
+# string to int
+c = int(b)
+print(c)
+# string to float
+d = float(b)
+print(d)
+# float to string
+e = str(d)
+print(e)
+# string to list
+f = "hello"
+g = list(f)
+print(g)
+# list to string
+h = ''.join(g)
+print(h)
