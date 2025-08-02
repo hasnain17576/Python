9459b2a583e77981c5f6ccf8e3672c98096afe24
hasnain17576 <hasnainwasli17@gmail.com>
2025-08-02 09:20:43 +0500

    submit

---
+++ Users input  in python/Users input.py
@@ -0,0 +1,9 @@
+a=input("Enter something: ")
+print("You entered:", a)
+a = input(" enter the first number: ")
+b = input(" enter the second number: ")
+c = int(a) + int(b)
+print("The sum is:", c)
+
+a = int(input("Enter a number: "))
+print("You entered:", a)
